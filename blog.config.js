b1c8e990b2c96ce2b93709f2f44ed60cf7f8274d
aleksandrmaas <55091655+aleksandrmaas@users.noreply.github.com>
2024-04-14 18:56:04 +0300

    Changed settings

--- blog.config.js
+++ blog.config.js
@@ -1,18 +1,18 @@
 const BLOG = {
-  title: "Sunrise AI RU",
+  title: "Sunrise AI На русском",
   author: "Aleks Sav",
   email: "i@craigary.net",
-  link: "https://nobelium.vercel.app",
-  description: "This gonna be an awesome website.",
-  lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
+  link: "https://sunai.ee/ru",
+  description: "Блог про искусственный интеллект",
+  lang: "ru", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: "Asia/Shanghai", // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: "auto", // ['light', 'dark', 'auto'],
   font: "sans-serif", // ['sans-serif', 'serif']
   lightBackground: "#ffffff", // use hex value, don't forget '#' e.g #fffefc
   darkBackground: "#18181B", // use hex value, don't forget '#'
   path: "", // leave this empty unless you want to deploy Nobelium in a folder
-  since: 2021, // If leave this empty, current year will be used.
-  postsPerPage: 7,
+  since: 2024, // If leave this empty, current year will be used.
+  postsPerPage: 10,
   sortByDate: false,
   showAbout: true,
   showArchive: true,
