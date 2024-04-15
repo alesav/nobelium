abb903dece25198e0070d024571255d8f1238ac4
aleksandrmaas <55091655+aleksandrmaas@users.noreply.github.com>
2024-04-15 08:25:37 +0300

    changed width

--- blog.config.js
+++ blog.config.js
@@ -1,9 +1,9 @@
 const BLOG = {
-  title: "Sunrise AI Blog EN",
+  title: "Sunrise AI Blog",
   author: "Aleks Sav",
-  email: "i@craigary.net",
+  email: "aleksandr@sunai.ee",
   link: "https://nobelium.vercel.app",
-  description: "This gonna be an awesome website.",
+  description: "about how artificial intelligence works",
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: "Asia/Shanghai", // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: "auto", // ['light', 'dark', 'auto'],
@@ -11,8 +11,8 @@ const BLOG = {
   lightBackground: "#ffffff", // use hex value, don't forget '#' e.g #fffefc
   darkBackground: "#18181B", // use hex value, don't forget '#'
   path: "", // leave this empty unless you want to deploy Nobelium in a folder
-  since: 2021, // If leave this empty, current year will be used.
-  postsPerPage: 7,
+  since: 2024, // If leave this empty, current year will be used.
+  postsPerPage: 10,
   sortByDate: false,
   showAbout: true,
   showArchive: true,
