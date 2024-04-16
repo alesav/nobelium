ce72b1fa766bcfbab474e720b95b393038e609cb
aleksandrmaas <55091655+aleksandrmaas@users.noreply.github.com>
2024-04-16 22:32:40 +0300

    First commit

--- blog.config.js
+++ blog.config.js
@@ -3,8 +3,8 @@ const BLOG = {
   author: "Aleks Sav",
   email: "aleksandr@sunai.ee",
   link: "https://nobelium.vercel.app",
-  description: "about how artificial intelligence works",
-  lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
+  description: "sobre el funcionamiento de la inteligencia artificial",
+  lang: "es-ES", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: "Asia/Shanghai", // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: "auto", // ['light', 'dark', 'auto'],
   font: "sans-serif", // ['sans-serif', 'serif']
