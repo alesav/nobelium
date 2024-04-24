929e83254fa0f4d1d514c612aea691c895471ee3
aleksandrmaas <55091655+aleksandrmaas@users.noreply.github.com>
2024-04-24 11:16:56 +0300

    Added counters

--- pages/_document.js
+++ pages/_document.js
@@ -1,25 +1,24 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
-import cn from 'classnames'
-import { config } from '@/lib/server/config'
-import tailwind from '@/tailwind.config'
-import CJK from '@/lib/cjk'
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import cn from "classnames";
+import { config } from "@/lib/server/config";
+import tailwind from "@/tailwind.config";
+import CJK from "@/lib/cjk";
 class MyDocument extends Document {
-  static async getInitialProps (ctx) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
   }
 
-  render () {
+  render() {
     const initialColorScheme = {
-      auto: 'color-scheme-unset',
-      dark: 'dark'
-    }[config.appearance]
+      auto: "color-scheme-unset",
+      dark: "dark",
+    }[config.appearance];
 
     return (
       <Html lang={config.lang} className={cn(initialColorScheme)}>
         <Head>
-          {config.font && config.font === 'serif'
-            ? (
+          {config.font && config.font === "serif" ? (
             <>
               <link
                 rel="preload"
@@ -36,8 +35,7 @@ class MyDocument extends Document {
                 crossOrigin="anonymous"
               />
             </>
-              )
-            : (
+          ) : (
             <>
               <link
                 rel="preload"
@@ -54,9 +52,9 @@ class MyDocument extends Document {
                 crossOrigin="anonymous"
               />
             </>
-              )}
+          )}
 
-          {['zh', 'ja', 'ko'].includes(
+          {["zh", "ja", "ko"].includes(
             config.lang.slice(0, 2).toLocaleLowerCase()
           ) && (
             <>
@@ -69,42 +67,59 @@ class MyDocument extends Document {
                 rel="preload"
                 as="style"
                 href={`https://fonts.googleapis.com/css2?family=Noto+${
-                  config.font === 'serif' ? 'Serif' : 'Sans'
+                  config.font === "serif" ? "Serif" : "Sans"
                 }+${CJK()}:wght@400;500;700&display=swap`}
               />
               <link
                 rel="stylesheet"
                 href={`https://fonts.googleapis.com/css2?family=Noto+${
-                  config.font === 'serif' ? 'Serif' : 'Sans'
+                  config.font === "serif" ? "Serif" : "Sans"
                 }+${CJK()}:wght@400;500;700&display=swap`}
               />
               <noscript>
                 <link
                   rel="stylesheet"
                   href={`https://fonts.googleapis.com/css2?family=Noto+${
-                    config.font === 'serif' ? 'Serif' : 'Sans'
+                    config.font === "serif" ? "Serif" : "Sans"
                   }+${CJK()}:wght@400;500;700&display=swap`}
                 />
               </noscript>
             </>
           )}
           <link rel="icon" href="/favicon.png" />
-          <link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="/feed"></link>
-          {config.appearance === 'auto'
-            ? (
+          <link
+            rel="alternate"
+            type="application/rss+xml"
+            title="RSS 2.0"
+            href="/feed"
+          ></link>
+          {config.appearance === "auto" ? (
             <>
-            <meta name="theme-color" content={config.lightBackground} media="(prefers-color-scheme: light)"/>
-            <meta name="theme-color" content={config.darkBackground} media="(prefers-color-scheme: dark)"/>
+              <meta
+                name="theme-color"
+                content={config.lightBackground}
+                media="(prefers-color-scheme: light)"
+              />
+              <meta
+                name="theme-color"
+                content={config.darkBackground}
+                media="(prefers-color-scheme: dark)"
+              />
             </>
-              )
-            : (
-            <meta name="theme-color" content={config.appearance === 'dark' ? config.darkBackground : config.lightBackground} />
-              )
-          }
+          ) : (
+            <meta
+              name="theme-color"
+              content={
+                config.appearance === "dark"
+                  ? config.darkBackground
+                  : config.lightBackground
+              }
+            />
+          )}
           {/* To ensure the initial background color follows media preference when ThemeProvider is
               not ready */}
           <style>
-          {`
+            {`
             .color-scheme-unset, .color-scheme-unset body {
               background-color: ${tailwind.theme.extend.colors.day.DEFAULT} !important;
             }
@@ -115,14 +130,56 @@ class MyDocument extends Document {
             }
           `}
           </style>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+                (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+      
+                ym(97116058, "init", {
+                      clickmap:true,
+                      trackLinks:true,
+                      accurateTrackBounce:true,
+                      webvisor:true
+                });
+              `,
+            }}
+          />
+          <noscript>
+            <div>
+              <img
+                src="https://mc.yandex.ru/watch/97116058"
+                style={{ position: "absolute", left: "-9999px" }}
+                alt=""
+              />
+            </div>
+          </noscript>
+
+          <span id="wts2157992"></span>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `  var wts=document.createElement(&quot;script&quot;);wts.async=true;
+          wts.src=&quot;https://app.ardalio.com/log7.js&quot;;document.head.appendChild(wts);
+          wts.onload = function(){wtslog7(2157992, 1)};`,
+            }}
+          />
+          <noscript>
+            <a href="https://www.web-stat.com">
+              <img
+                src="https://app.ardalio.com/7/1/2157992.png"
+                alt="Web-Stat web statistics"
+              />
+            </a>
+          </noscript>
         </Head>
         <body className="bg-day dark:bg-night">
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
 
-export default MyDocument
+export default MyDocument;
